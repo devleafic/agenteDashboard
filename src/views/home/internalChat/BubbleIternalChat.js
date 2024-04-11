501919d3712e14d2d67e35f6df24ad9b0b9ad466
Gabriel Jiménez <jhon2k1@gmail.com>
2024-04-11 12:39:31 -0600

    fix classes bubles internal chat

--- src/views/home/internalChat/BubbleIternalChat.js
+++ src/views/home/internalChat/BubbleIternalChat.js
@@ -1,5 +1,5 @@
 import { useRef, useEffect, useState } from 'react';
-import {Dropdown} from 'semantic-ui-react';
+import {Dropdown, Icon, Image, Label} from 'semantic-ui-react';
 import { useSocket } from '../../../controladores/InternalChatContext';
 
 export default function BubbleIternalChat({infoChat, msg, userInfo, readMessage}) {
@@ -29,7 +29,7 @@ export default function BubbleIternalChat({infoChat, msg, userInfo, readMessage}
         });
 
         return <div style={{display:'flex', justifyContent: direcction === 'left' ? 'end' : 'start'}}>{Object.keys(reactionCount).map((reaction) => {
-            return <div key={reaction} style={{marginRight:5}}>{reaction} {reactionCount[reaction]}</div>
+            return <div key={reaction} style={{padding:10}}>{reaction} {reactionCount[reaction]}</div>
         })}</div>;
     
     }
@@ -70,20 +70,60 @@ export default function BubbleIternalChat({infoChat, msg, userInfo, readMessage}
         return text.replace(urlRegex, (url) => `<a href="${url}" target="_blank">${url}</a>`);
     };
 
+    const convertContent = (msg) => {
+        const type = msg.typeMessage;
+        const content = msg.message;
+       
+        switch(type){
+            case 'text':
+                return <div className="internal-chat-message" dangerouslySetInnerHTML={{ __html: formatMessage(content) }}></div>;
+            case 'document':
+                return (<a target='blank' href={content}><Icon name='folder open outline'></Icon></a>);
+            case 'image':
+               return (<div className="internal-chat-message" ><a href={content} target='_blank'><Image style={{borderRadius: '8px'}} size='small' src={content}/></a></div>);
+            case 'sticker':
+                return (<><a href={content} target='_blank'><Image src={content} style={{borderRadius: '8px'}} size='small' /></a></>);
+            case 'video':
+                return (<video controls><source src={content} type='video/mp4' style={{borderRadius: '8px' }}  reload='auto'/></video>)
+            case 'voice':
+                return (<audio controls >   
+                    <source src={content} type='audio/ogg' />   
+                    <source src={content} type='audio/mpeg' />   
+                </audio> )
+            case 'call':   
+            return (  (<audio controls >   
+                <source src={content} type='audio/ogg' />   
+                <source src={content} type='audio/mpeg' />   
+            </audio>) )                 
+            case 'externalAttachment' :
+                return (<video controls><source src={content} type='video/mp4' style={{borderRadius: '15px' }}  reload='auto'/></video>)
+            case 'notify':
+                return <Label color="red"> <Icon name='x' />{content}<Label.Detail>FINALIZAR LA CONVERSACIÓN</Label.Detail></Label>          
+            case 'errors':
+                return (<>[{type}] - {content}</>);      
+            default:
+                return (<>[La clase {type} no esta soportada] - {content}</>);
+        }
+    }
 
   return (<>
-    <div key={msg._id} className={userInfo._id !== msg.createdBy ? 'internal-chat-received' : 'internal-chat-sent'} ref={messageRef} data-message-id={msg._id}>
-        <div className="internal-chat-message" dangerouslySetInnerHTML={{ __html: formatMessage(msg.message) }}></div>
+    <div key={msg._id} className={userInfo._id !== msg.createdBy ? 'internal-chat-received' : 
+        'internal-chat-sent'} ref={messageRef} data-message-id={msg._id}>
+        {convertContent(msg)}
+        {/*<div className="internal-chat-message" dangerouslySetInnerHTML={{ __html: formatMessage(msg.message) }}></div>*/}
         {userInfo._id === msg.createdBy && (msg.readers.length > 1) && <div>Leido</div>}
         <div>
             {
                 renderAndCountReactions(msg.reactions, userInfo._id !== msg.createdBy ? 'right' : 'left')
             }
         </div>
-        <Dropdown text='Reaccionar' onChange={sendReaction} options={[
+        <Dropdown style={{padding : 15}} text='💬' onChange={sendReaction} options={[
+            {key : 'emoji-0', text : '🙂', value : '🙂'},
             {key : 'emoji-1', text : '🤔', value : '🤔'},
-            {key : 'emoji-2', text : '😁', value : '😁'},
+            {key : 'emoji-2', text : '😡', value : '😡'},
+            {key : 'emoji-2', text : '😳', value : '😳'},
             {key : 'emoji-3', text : '👍', value : '👍'},
+            {key : 'emoji-4', text : '👎', value : '👎'},
         ]} name='reaction'/>
     </div>
     </>)
